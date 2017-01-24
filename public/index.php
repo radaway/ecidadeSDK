271a4ee33e6165c7e265f79256cb982b04c04e19
Radamés Kremer <radames@dbseller.com.br>
2017-01-23 23:37:09 -0200

    ajustes

--- public/index.php
+++ public/index.php
@@ -3,6 +3,5 @@ require_once __DIR__ . "/../src/Routes/Route.php";
 $Route = new Route();
 $Route->add('/', 'Home');
 $Route->add('/build', 'Build');
-$Route->add('/build/{name}', 'Build');
 $Route->get();
 ?>
