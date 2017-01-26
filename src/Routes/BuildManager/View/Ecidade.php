c8224371e99534b3fa614e4c9b0e064605f3a745
Radamés Kremer <radames@dbseller.com.br>
2017-01-26 13:16:55 -0200

    ajustes

--- src/Routes/BuildManager/View/Ecidade.php
+++ src/Routes/BuildManager/View/Ecidade.php
@@ -1,8 +1,15 @@
 <?php
 
 class Ecidade{
-  public function __construct( $BuildName, $Method  ){
-    echo "teste novo";
+  private $buildName;
+  public function __construct( $buildName, $Method  ){
+    $this->buildName = $buildName;
+    echo $this->getInfo();
+  }
+
+  private function getInfo(){
+    $dockerPort = file_get_contents( '/var/www/builds/' . $this->buildName . '/builds/EcidadeOnline2_ports.conf');
+    return 'http://' . $_SERVER['SERVER_NAME'] . ':' . $dockerPort;
   }
 }
 
--- src/Routes/BuildManager/View/EcidadeOnline2.php
+++ src/Routes/BuildManager/View/EcidadeOnline2.php
@@ -1,9 +1,17 @@
 <?php
 
 class EcidadeOnline2{
-  public function __construct( $BuildName, $Method  ){
-    echo "teste novo mimim";
+  private $buildName;
+  public function __construct( $buildName, $Method  ){
+    $this->buildName = $buildName;
+    echo $this->getInfo();
   }
+
+  private function getInfo(){
+    $dockerPort = file_get_contents( '/var/www/builds/' . $this->buildName . '/builds/EcidadeOnline2_ports.conf');
+    return 'http://' . $_SERVER['SERVER_NAME'] . ':' . $dockerPort;
+  }
+
 }
 
  ?>
