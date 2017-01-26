b0d3a28c678712d424d7d167d02f83b1dfb3ce57
Radamés Kremer <radames@dbseller.com.br>
2017-01-26 10:54:24 -0200

    ajustes

--- src/Routes/BuildManager/BuildManager.php
+++ src/Routes/BuildManager/BuildManager.php
@@ -3,8 +3,56 @@
 class BuildManager{
 
   public function __construct( $buildName ){
-    $lsDir = array_diff( scandir( '/var/www/builds/' . $buildName . '/builds'), array( '.', '..' ) );
-    print_r($lsDir);
+    if (isset( $_POST['view'] )){
+      if ( is_file( __DIR__ . "/View/" . $_POST['view'] .".php" ) ){
+        require_once __DIR__ . "/View/" . $_POST['view'] .".php";
+        return new $_POST['view']( $buildName,  $_POST['method'] );
+      }
+    }
+    if (isset( $_POST['ctrl'] )){
+      if ( is_file( __DIR__ . "/Controller/" . $_POST['ctrl'] .".php" ) ){
+        require_once __DIR__ . "/Controller/" . $_POST['ctrl'] .".php";
+        return new $_POST['ctrl']( $buildName, $_POST['method'] );
+      }
+    }
+    echo $this->indexPage();
+  }
+
+  private function indexPage(){
+    return '
+<html>
+  <head>
+    <title>e-cidade SDK</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="Expires" CONTENT="0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" media="screen"  />
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+  </head>
+  <body>
+    <div style="width: 98%;" class="container" id="exibicao" align="center"></div>
+    <script type="text/javascript">
+      function LoadView(View, Method){
+        $("#exibicao").html("<br /><div align=\"center\">Carregando!!!</div>");
+        $.ajax({
+          data: {view: View, method: Method},
+          type: "POST",
+     		  url: "' . $_SERVER['REQUEST_URI'] . '",
+     		  success: function(html){
+     		     $("#exibicao").html(html);
+     		  }
+     		});
+      }
+    </script>
+    <script type="text/javascript">
+    	$(document).ready( function() {
+    		  LoadView("BuildInfo", "info");
+      });
+    </script>
+  </body>
+</html>';
   }
 
 }
---
+++ src/Routes/BuildManager/View/BuildInfo.php
@@ -0,0 +1,43 @@
+<?php
+
+class BuildInfo{
+
+    private $Size = 10;
+
+    public function __construct( $BuildName, $Method ){
+      switch ($Method) {
+        case 'info':
+          echo $this->info( $BuildName );
+          break;
+        default:
+          echo "OPCAO INVALIDA";
+          break;
+      }
+    }
+
+
+    private function info( $BuildName ){
+      $html = '<div class="container" align="center">
+      <br /><div class="col-' . $this->Size . '">
+      ';
+
+      $lsDir = array_diff( scandir( '/var/www/builds/' . $BuildName . '/builds' ), array( '.', '..' ) );
+      foreach ($lsDir as $value) {
+        if ( ! is_dir( '/var/www/builds/' . $BuildName . '/builds/' . $value ) ){
+          continue;
+        }
+        $html .= '<div class="card card-default">
+        <div class="card-header">
+        ';
+        $html .= '<strong>' . $value . '</strong>
+        </div>
+        <div class="card-body"></div></div>';
+      }
+      $html .= '</div></div>';
+      return $html;
+
+    }
+
+}
+
+ ?>
