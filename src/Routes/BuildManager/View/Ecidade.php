0b36049861fbe3ef05618190ebe115391acab40c
Radamés Kremer <radames@dbseller.com.br>
2017-01-26 15:02:38 -0200

    ajustes

--- src/Routes/BuildManager/View/Ecidade.php
+++ src/Routes/BuildManager/View/Ecidade.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . '/../../../Html/SimpleTable.class.php';
 class Ecidade{
   private $buildName;
   public function __construct( $buildName, $Method  ){
@@ -9,7 +9,13 @@ class Ecidade{
 
   private function getInfo(){
     $dockerPort = file_get_contents( '/var/www/builds/' . $this->buildName . '/builds/Ecidade_ports.conf');
-    return '<br/>http://' . $_SERVER['SERVER_NAME'] . ':' . $dockerPort . '<br/>';
+    $table = new SimpleTable( 'Table' );
+    $table->addHead( array( 'Serviço', 'Acesso' ) );
+    $table->addline( array( 'e-cidade', '<a href="http://' . $_SERVER['SERVER_NAME'] . ':' . $dockerPort . '" target="_blank">http://' . $_SERVER['SERVER_NAME'] . ':' . $dockerPort . '</a>' ) );
+    $table->addline( array( 'git', 'http://' . $_SERVER['SERVER_NAME'] . ':' . $dockerPort . '/_git' ) );
+    $table->addline( array( 'ssh', 'http://' . $_SERVER['SERVER_NAME'] . ':' . $dockerPort . '/_ssh' ) );
+    return $table->print();
+
   }
 }
 
