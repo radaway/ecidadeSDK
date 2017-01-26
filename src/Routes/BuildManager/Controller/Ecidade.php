cb6a85bdda4d9d10c0d6c8146a393622abf18cc7
Radamés Kremer <radames@dbseller.com.br>
2017-01-26 15:50:45 -0200

    ajustes

---
+++ src/Routes/BuildManager/Controller/Ecidade.php
@@ -0,0 +1,48 @@
+<?php
+require_once __DIR__ . '/../../../Docker/class/Docker.class.php';
+class Ecidade{
+
+  private $buildName;
+
+  public function __construct( $buildName, $Method ){
+    $this->buildName = $buildName;
+    switch ($Method) {
+      case 'dockerStop':
+        echo $this->dockerStop();
+        break;
+      case 'dockerStart':
+        echo $this->dockerStart();
+      default:
+        echo "bugou";
+        break;
+    }
+  }
+
+  private function dockerStop(){
+    $dockerPort = file_get_contents( '/var/www/builds/' . $this->buildName . '/builds/Ecidade_ports.conf');
+    $dockerPort = trim( $dockerPort );
+    try {
+      $docker = new Docker();
+      $docker->stop( $dockerPort );
+    } catch (Exception $e) {
+      return $e->getMessage();
+    }
+    return 'Serviço docker parado';
+  }
+
+  private function dockerStart(){
+    $dockerPort = file_get_contents( '/var/www/builds/' . $this->buildName . '/builds/Ecidade_ports.conf');
+    $dockerPort = trim( $dockerPort );
+    try {
+      $docker = new Docker();
+      $docker->start( 'apache_ecidade', '/var/www/builds/' . $this->buildName . '/builds/Ecidade', $dockerPort );
+    } catch (Exception $e) {
+      return $e->getMessage();
+    }
+    return 'Serviço docker iniciado';
+  }
+
+
+}
+
+?>
