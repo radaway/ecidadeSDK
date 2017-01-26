67d5f0e464d4184c1652d42ec2b273d450295c49
Radamés Kremer <radames@dbseller.com.br>
2017-01-26 10:57:06 -0200

    ajustes

--- src/Routes/BuildManager/BuildManager.php
+++ src/Routes/BuildManager/BuildManager.php
@@ -19,40 +19,40 @@ class BuildManager{
   }
 
   private function indexPage(){
-    return '
-<html>
-  <head>
-    <title>e-cidade SDK</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="Expires" CONTENT="0">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" media="screen"  />
-    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-  </head>
-  <body>
-    <div style="width: 98%;" class="container" id="exibicao" align="center"></div>
-    <script type="text/javascript">
-      function LoadView(View, Method){
-        $("#exibicao").html("<br /><div align=\"center\">Carregando!!!</div>");
-        $.ajax({
-          data: {view: View, method: Method},
-          type: "POST",
-     		  url: "' . $_SERVER['REQUEST_URI'] . '",
-     		  success: function(html){
-     		     $("#exibicao").html(html);
-     		  }
-     		});
-      }
-    </script>
-    <script type="text/javascript">
-    	$(document).ready( function() {
-    		  LoadView("BuildInfo", "info");
-      });
-    </script>
-  </body>
-</html>';
+    $html = '<html>
+      <head>
+        <title>e-cidade SDK</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="Expires" CONTENT="0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" media="screen"  />
+        <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+      </head>
+      <body>
+        <div style="width: 98%;" class="container" id="exibicao" align="center"></div>
+        <script type="text/javascript">
+          function LoadView(View, Method){
+            $("#exibicao").html("<br /><div align=\"center\">Carregando!!!</div>");
+            $.ajax({
+              data: {view: View, method: Method},
+              type: "POST",
+         		  url: "' . $_SERVER['REQUEST_URI'] . '",
+         		  success: function(html){
+         		     $("#exibicao").html(html);
+         		  }
+         		});
+          }
+        </script>
+        <script type="text/javascript">
+        	$(document).ready( function() {
+        		  LoadView("BuildInfo", "info");
+          });
+        </script>
+      </body>
+    </html>';
+    return $html;
   }
 
 }
