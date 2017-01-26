f5e7379949d4f37949fb8454fefa32c5264e741d
Radamés Kremer <radames@dbseller.com.br>
2017-01-26 11:03:11 -0200

    ajustes

--- src/Routes/BuildManager/BuildManager.php
+++ src/Routes/BuildManager/BuildManager.php
@@ -25,10 +25,10 @@ class BuildManager{
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta http-equiv="Expires" CONTENT="0">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/font-awesome.min.css">
-        <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" media="screen"  />
-        <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
-        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <link rel="stylesheet" href="../css/font-awesome.min.css">
+        <link type="text/css" href="../css/bootstrap.min.css" rel="stylesheet" media="screen"  />
+        <script type="text/javascript" src="../js/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="../js/bootstrap.js"></script>
       </head>
       <body>
         <div style="width: 98%;" class="container" id="exibicao" align="center"></div>
