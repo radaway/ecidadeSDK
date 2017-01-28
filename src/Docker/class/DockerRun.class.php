cf5a328d74fd2770eca3975fe92a3296a1daba44
Radamés Kremer <radames@dbseller.com.br>
2017-01-28 14:43:40 -0200

    ajustes

--- src/Docker/class/DockerRun.class.php
+++ src/Docker/class/DockerRun.class.php
@@ -1,16 +1,7 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/DockerRequest.class.php';
-require_once __DIR__ . '/DockerJsonCreate.class.php';
 class DockerRun extends DockerRequest{
-
-  private $image = null;
-  private $containerId = null;
-
-  public function __construct() {
-    $this->image = $dockerImage;
-  }
-
   private function create( $dockerJson ){
     try {
       $result = $this->containerRequest( null, 'create', $dockerJson );
